8a40c92a524aeb16d01e1eafd44289a4c7c3d8dc
Bohdan Tarasiuk <bohdan.tarasiuk@smile-ukraine.com>
2022-04-07 18:36:43 +0300

    Create base structure and set base settings

---
+++ public/config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('VG_ACCESS') or die('Access denied.');
+
+const SITE_URL = 'http://ecomm-mvc.loc:8080';
+const PATH = '/';
+
+const HOST = 'laravel-mysql';
+const USER = 'root';
+const PASS = 'dbroot';
+const DB_NAME = 'ecomm_mvc';
